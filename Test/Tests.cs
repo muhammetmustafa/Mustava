86dee3770b163013c3d6fb6fbd6588f5bbdf4c57
LDC <LDC@DESKTOP-OKMQ0ND>
2017-08-17 09:23:09 +0300

    Lots of restructuring, addition, movings.

---
+++ Test/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Mustava.Attributes;
+using Mustava.Extensions;
+using NUnit.Framework;
+
+namespace Test
+{
+    public enum MyEnum
+    {
+        [Symbol("amu")]
+        Bir,
+        
+        [Symbol("amm")]
+        Iki,
+        
+        Uc
+    }
+    
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var str = "34mn,,,mlmlm";
+            Console.WriteLine(str.Split(new []{','}, StringSplitOptions.RemoveEmptyEntries).Concatenate("."));
+        }
+    }
+}
